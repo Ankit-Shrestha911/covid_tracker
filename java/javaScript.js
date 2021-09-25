ea9f3f221fb858e8a3d20bbd0fba854642d5c2af
Ankit Shrestha <74950690+Ankit-Shrestha911@users.noreply.github.com>
2021-09-25 13:46:30 +0530

    Add files via upload

---
+++ java/javaScript.js
@@ -0,0 +1,40 @@
+
+
+const recovered=document.querySelector('#recover');
+const confirm=document.querySelector('#confirm');
+const death=document.querySelector('#death');
+const active=document.querySelector('#active');
+const update=document.querySelector('#update');
+
+
+
+const res=new XMLHttpRequest();
+res.open('GET', "https://cors-anywhere.herokuapp.com/https://api.covid19india.org/data.json");
+res.send();
+
+
+async function getCovidData() {
+    const res=await fetch("https://cors-anywhere.herokuapp.com/https://api.covid19india.org/data.json");
+    const data=await res.json();
+
+    const india=data.statewise[0];
+    console.log(india);
+    recovered.innerHTML="Loading...";
+    confirm.innerHTML="Loading...";
+    death.innerHTML="Loading...";
+    active.innerHTML="Loading...";
+    update.innerHTML="Loading...";
+
+    setTimeout(() => {
+        recovered.innerHTML=india.recovered.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+        confirm.innerHTML=india.confirmed.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+        death.innerHTML=india.deaths.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+        active.innerHTML=india.active.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+        update.innerHTML=india.lastupdatedtime;
+    }, 2000);
+
+
+}
+
+res.addEventListener('load', getCovidData);
+
